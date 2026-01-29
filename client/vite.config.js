5047b73d9f09285adef9e5ec02e424dde8fa1530
Tanzeem Ahmad <ahmadtanzeem185@gmail.com>
2026-01-29 15:27:24 +0530

    remove proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,15 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://library-management-system-2yg5.onrender.com",
-        changeOrigin: true,
-        secure:true,
-      }
-    }
-  }
 })
 
 
