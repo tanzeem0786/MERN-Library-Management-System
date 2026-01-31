12964edaecaef00a102b2cf28faf7b8af5b53b70
Tanzeem Ahmad <ahmadtanzeem185@gmail.com>
2026-01-31 10:35:16 +0530

    fix some issue

--- Server/middlewares/authMIddlewares.js
+++ Server/middlewares/authMIddlewares.js
@@ -4,7 +4,7 @@ import ErrorHandler from "./errorMiddlewares.js";
 import  jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async(req, res, next) => {
-    const {token} = req.cookies.token;
+    const {token} = req.cookies;
     if(!token) {
         return next(new ErrorHandler("User is not Authenticated!", 400));
     }
